3542262285c84dfd0f07d11094d7953400580154
waamber <amberastuart@aol.com>
2018-02-10 10:52:50 -0600

    name parser testing

---
+++ Stuff.Tests/NameParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Stuff.Tests
+{
+    public class NameParser
+    {
+        internal NameParser Parse(string name)
+        {
+            var nameParts = name.Split(' ');
+            var result = new Name();
+
+            switch (nameParts.Length)
+            {
+                case 1:
+                    result = new Name
+                    {
+                        FirstName = name
+                    };
+                    break;
+
+                case 2:
+                    result = new Name
+                    {
+                        FirstName = nameParts[0],
+                        LastName = nameParts[1]
+                    };
+                    break;
+
+                case 3:
+                    result = new Name
+                    {
+                        FirstName = nameParts[0],
+                        MiddleName = nameParts[1],
+                        LastName = nameParts[2]
+                    };
+                    break;
+            }
+            return result;
+        }
+    }
+
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
